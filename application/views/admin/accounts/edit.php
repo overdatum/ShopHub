6b5e5c225db940b3a3ae9fd01812e5c1f2e1c743
Koen Schmeets <k.schmeets@gmail.com>
2012-01-27 21:28:38 +0100

    Changing form layour

--- application/views/admin/accounts/edit.php
+++ application/views/admin/accounts/edit.php
@@ -1,8 +1,8 @@
 <div id="main">
-	<?= Form::open('admin/accounts/edit/'.$account->id, 'PUT') ?>
+	<?= Form::open('admin/accounts/edit/'.$account->id, 'PUT', array('class' => 'form-horizontal')) ?>
 		<?= Form::field('text', 'name', 'Name', array(Input::old('name', $account->name)), array('error' => $errors->first('name'))) ?>
 		<?= Form::field('text', 'email', 'E-mail address', array(Input::old('email', $account->email)), array('error' => $errors->first('email'))) ?>
 		<?= Form::field('text', 'password', 'New password', array(), array('error' => $errors->first('password'))) ?>
-		<?= Form::actions(array(Form::submit('Edit account', array('class' => 'btn large primary')))) ?>
+		<?= Form::actions(array(Form::submit('Save changes', array('class' => 'btn large primary')))) ?>
 	<?= Form::close() ?>
 </div>
