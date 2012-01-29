24ac091981d58991d81aedf9c0d1469711868835
Koen Schmeets <k.schmeets@gmail.com>
2012-01-29 02:25:54 +0100

    Whitespace

--- application/models/role.php
+++ application/models/role.php
@@ -1,6 +1,6 @@
 <?php
 class Role extends Eloquent\Model {
-	
+
 	public function accounts()
 	{
 		return $this->has_and_belongs_to_many('Account');
@@ -12,7 +12,7 @@ class Role extends Eloquent\Model {
 					->where_role_id($this->id)
 					->where_language_id(Auth::user()->language_id)
 					->first();
-		
+
 		return $lang;
 	}
 
