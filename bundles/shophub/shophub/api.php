5eca1323fca0cdf5f4f43f0d6a16c349944f1c97
Koen Schmeets <k.schmeets@gmail.com>
2012-04-22 18:08:41 +0200

    Changes to API class
    Signed-off-by: Koen Schmeets <k.schmeets@gmail.com>

--- bundles/shophub/shophub/api.php
+++ bundles/shophub/shophub/api.php
@@ -6,93 +6,65 @@ use Laravel\Config;
 use Laravel\Routing\Route;
 use Laravel\Response;
 
-use API\Drivers\Memory;
-use API\Drivers\HTTP;
-
 class API {
 
-	/**
-	 * All of the active API drivers.
-	 *
-	 * @var array
-	 */
-	public static $drivers = array();
-
-	public static function serve($prefix = 'api')
+	public static function call($method, $arguments, $input = array(), $segments = array())
 	{
-		Route::any($prefix . '/(:any?)/(:any?)/(:all?)', function($alias, $method, $arguments = '')
+		$url = 'http://local.shophub.io/api/' . implode('/', $arguments) . (count($segments) > 0 ? '?' . http_build_query($segments) : '');
+
+		$headers = array(
+			'Accept: application/json',
+			'Content-Type: application/json',
+		);
+
+		$data = json_encode($input);
+
+		$ch = curl_init();
+		//curl_setopt($ch, CURLOPT_USERPWD, "loginname:passwort");
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+		if(strtoupper($method) != 'GET')
 		{
-			$repositories = Config::get('shophub::api.repositories');
-			$class = new $repositories[$alias];
-			$arguments = explode('/', $arguments);
+			if(strtoupper($method) != 'DELETE')
+			{
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+			}
 
-			return Response::make(json_encode(call_user_func_array(array($class, $method), $arguments)), 200, array('content-type: application/json'));
-		});
-	}
-
-	/**
-	 * Get a API driver instance.
-	 *
-	 * If no driver name is specified, the default will be returned.
-	 *
-	 * <code>
-	 *		// Get the default cache driver instance
-	 *		$driver = API::driver();
-	 *
-	 *		// Get a specific cache driver instance by name
-	 *		$driver = API::driver('http');
-	 * </code>
-	 *
-	 * @param  string        $driver
-	 * @return API\Drivers\Driver
-	 */
-	public static function driver($driver = '')
-	{
-		if (is_null($driver)) $driver = Config::get('shophub::api.driver');
-
-		if ( ! isset(static::$drivers[$driver]))
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		}
+		elseif(count($segments) > 0)
 		{
-			static::$drivers[$driver] = static::factory($driver);
+
 		}
 
-		return static::$drivers[$driver];
+		$response = curl_exec($ch);
+		$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+		return json_decode($response);
 	}
 
-	/**
-	 * Create a new cache driver instance.
-	 *
-	 * @param  string  $driver
-	 * @return Cache\Drivers\Driver
-	 */
-	protected static function factory($driver)
+	public static function get($arguments, $segments = array())
 	{
-		switch ($driver)
-		{
-			case 'memory':
-				return new API\Drivers\Memory;
-			break;
-
-			case 'http':
-				return new API\Drivers\HTTP;
-			break;
-
-			default:
-				throw new Exception("API driver {$driver} is not supported.");
-			break;
-		}
+		return static::call('GET', $arguments, array(), $segments);
 	}
 
-	/**
-	 * Magic Method for calling the methods on the default API driver.
-	 *
-	 * <code>
-	 *		// Call the "call" method on the default API driver
-	 *		$name = API::call(array('users', 'all'));
-	 * </code>
-	 */
-	public static function __callStatic($method, $parameters)
+	public static function post($arguments, $input = array())
 	{
-		return call_user_func_array(array(static::driver(), $method), $parameters);
+		return static::call('POST', $arguments, $input);
+	}
+
+	public static function put($arguments, $input = array())
+	{
+		return static::call('PUT', $arguments, $input);
+	}
+
+	public static function delete($arguments, $input = array())
+	{
+		return static::call('DELETE', $arguments, $input);
 	}
 
 }
