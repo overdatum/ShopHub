278448767ff55d64252441e720509277018124e2
Koen Schmeets <k.schmeets@gmail.com>
2012-04-23 05:27:19 +0200

    Updating API class

--- bundles/shophub/shophub/api.php
+++ bundles/shophub/shophub/api.php
@@ -12,9 +12,11 @@ class API {
 	{
 		$url = 'http://local.shophub.io/api/' . implode('/', $arguments) . (count($segments) > 0 ? '?' . http_build_query($segments) : '');
 
+		$method = strtoupper($method);
+
 		$headers = array(
-			'Accept: application/json',
-			'Content-Type: application/json',
+			'accept: application/json',
+			'content-type: application/json',
 		);
 
 		$data = json_encode($input);
@@ -26,25 +28,40 @@ class API {
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-		if(strtoupper($method) != 'GET')
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+		
+		if($method !== 'GET')
 		{
-			if(strtoupper($method) != 'DELETE')
-			{
-				curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-			}
-
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-		}
-		elseif(count($segments) > 0)
-		{
-
+ 			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 		}
 
 		$response = curl_exec($ch);
 		$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-		return json_decode($response);
+		switch ($code) {
+			case 401:
+				throw new Exception("401 Unauthorized");	
+			break;
+
+			case 404:
+				throw new Exception("404 Not Found: We couldn't find the resource you're looking for. Please check the documentation and try again");
+			break;
+
+			case 500:
+				throw new Exception("500 Internal Server Error: Sorry, we've run into a problem. Please try again or contact support");
+			break;
+			
+			case 400:
+				return (object) array(
+					'code' => 400,
+					'errors' => json_decode($response)
+				);
+			break;
+
+			default:
+				return json_decode($response);
+			break;
+		}
 	}
 
 	public static function get($arguments, $segments = array())
