47e1795b1c3e5b61bcb38a97be0cb6597ad2fbda
Koen Schmeets <k.schmeets@gmail.com>
2012-04-23 05:27:49 +0200

    Adding pretty=1 option to API requests

--- bundles/thirdparty/service/start.php
+++ bundles/thirdparty/service/start.php
@@ -4,6 +4,62 @@ Autoloader::map(array(
 	'Service' => __DIR__ . DS . 'service' . EXT
 ));
 
+function prettify_json($json) {
+	$result = '';
+	$pos = 0;
+	$strLen = strlen($json);
+	$indentStr = "\t";
+	$newLine = "\n";
+	$prevChar = '';
+	$outOfQuotes = true;
+
+	for($i = 0; $i <= $strLen; $i++)
+	{
+		// Grab the next character in the string
+		$char = substr($json, $i, 1);
+
+		// Are we inside a quoted string?
+		if($char == '"' && $prevChar != '\\')
+		{
+			$outOfQuotes = !$outOfQuotes;
+		}
+		// If this character is the end of an element, 
+		// output a new line and indent the next line
+		else if(($char == '}' || $char == ']') && $outOfQuotes)
+		{
+			$result .= $newLine;
+			$pos --;
+			for ($j=0; $j<$pos; $j++)
+			{
+				$result .= $indentStr;
+			}
+		}
+		// Add the character to the result string
+		$result .= $char;
+
+		// If the last character was the beginning of an element, 
+		// output a new line and indent the next line
+		if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes)
+		{
+			$result .= $newLine;
+			if ($char == '{' || $char == '[')
+			{
+				$pos ++;
+			}
+			
+			for ($j = 0; $j < $pos; $j++)
+			{
+				$result .= $indentStr;
+			}
+		}
+
+		$prevChar = $char;
+	}
+
+	return $result;
+}
+
+
 /**
  * Register a JSON service
  * 
@@ -12,12 +68,12 @@ Autoloader::map(array(
  */
 Service::register('json', function(Service $service)
 {
-	if(false)
-	{
-		return "<script>var test = " . json_encode($service->data) . "; console.log(test);</script>";
-	}
-	
 	$service->header('Content-Type', 'application/json');
 
+	if(Input::get('pretty'))
+	{
+		return prettify_json(json_encode($service->data));
+	}
+	
 	return json_encode($service->data);
 });
