dbff545ae5873f73430208c2248a7bec67d8da4f
Koen Schmeets <k.schmeets@gmail.com>
2012-04-24 11:04:01 +0200

    Adding is_uuid helper

--- bundles/shophub/shophub/helpers.php
+++ bundles/shophub/shophub/helpers.php
@@ -1,5 +1,10 @@
 <?php
 
+function is_uuid($uuid)
+{
+     return (bool) preg_match('/^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/i', $uuid);
+}
+
 function array_pluck($models, $value, $key = null)
 {
 	$result = array();
